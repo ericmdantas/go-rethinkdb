5fa4dc020a49457704150e8b89bf331ef85a69b1
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-06-10 07:46:51 -0300

    .

--- r.go
+++ r.go
@@ -15,21 +15,28 @@ func session() (*r.Session, error) {
 }
 
 func create() {
-	s, _ := session()
-
-	r.DBCreate("ex").Run(s, r.RunOpts{})
+	//s, _ := session()
 }
 
 func read() {
 	s, _ := session()
-	
-	t, err := r.Table("todo").Run(s)
-	
+	res, err := r.Table("2do").Filter(map[string]string{"msg": "2"}, r.FilterOpts{}).Run(s)
+
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	
-	fmt.Println(t)
+
+	var rows []map[string]interface{}
+
+	err = res.All(&rows)
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	for _, v := range rows {
+		fmt.Println(v["msg"])
+	}
 }
 
 func main() {
